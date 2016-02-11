4b44c088817e144ac10d4e360d32ba12f2f80077
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-02-11 11:50:00 +0700

    Add subcommand: distscripts

---
+++ lib/App/lcpan/Cmd/distscripts.pm
@@ -0,0 +1,52 @@
+package App::lcpan::Cmd::distscripts;
+
+# DATE
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+
+require App::lcpan;
+
+our %SPEC;
+
+$SPEC{'handle_cmd'} = {
+    v => 1.1,
+    summary => 'List scripts in a distribution',
+    args => {
+        %App::lcpan::common_args,
+        %App::lcpan::dist_args,
+        %App::lcpan::detail_args,
+    },
+};
+sub handle_cmd {
+    my %args = @_;
+
+    my $state = App::lcpan::_init(\%args, 'ro');
+    my $dbh = $state->{dbh};
+
+    my $dist = $args{dist};
+    my $detail = $args{detail};
+
+    my $sth = $dbh->prepare("SELECT
+  script.name name,
+  script.abstract abstract
+FROM script
+LEFT JOIN file ON script.file_id=file.id
+LEFT JOIN dist ON file.id=dist.file_id
+WHERE dist.name=?
+ORDER BY name DESC");
+    $sth->execute($dist);
+    my @res;
+    while (my $row = $sth->fetchrow_hashref) {
+        push @res, $detail ? $row : $row->{name};
+    }
+    my $resmeta = {};
+    $resmeta->{'table.fields'} = [qw/name abstract/]
+        if $detail;
+    [200, "OK", \@res, $resmeta];
+}
+
+1;
+# ABSTRACT:
