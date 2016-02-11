2855fbc8031963e4b26396aeff16c51583c398b7
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-02-11 11:58:45 +0700

    Add subcommand: authors-by-script-count

---
+++ lib/App/lcpan/Cmd/authors_by_script_count.pm
@@ -0,0 +1,48 @@
+package App::lcpan::Cmd::authors_by_script_count;
+
+# DATE
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+
+require App::lcpan;
+
+our %SPEC;
+
+$SPEC{'handle_cmd'} = {
+    v => 1.1,
+    summary => 'List authors ranked by number of modules',
+    args => {
+        %App::lcpan::common_args,
+    },
+};
+sub handle_cmd {
+    my %args = @_;
+
+    my $state = App::lcpan::_init(\%args, 'ro');
+    my $dbh = $state->{dbh};
+
+    my $sql = "SELECT
+  file.cpanid author,
+  COUNT(*) AS script_count
+FROM script
+LEFT JOIN file ON script.file_id=file.id
+GROUP BY file.cpanid
+ORDER BY script_count DESC
+";
+
+    my @res;
+    my $sth = $dbh->prepare($sql);
+    $sth->execute();
+    while (my $row = $sth->fetchrow_hashref) {
+        push @res, $row;
+    }
+    my $resmeta = {};
+    $resmeta->{'table.fields'} = [qw/author script_count/];
+    [200, "OK", \@res, $resmeta];
+}
+
+1;
+# ABSTRACT:
