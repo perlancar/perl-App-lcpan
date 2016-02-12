310cbfae93254a8e321d14483a00f27cf69e9049
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-12 12:31:58 +0700

    script2dist: Add option --all

--- lib/App/lcpan/Cmd/script2dist.pm
+++ lib/App/lcpan/Cmd/script2dist.pm
@@ -17,6 +17,7 @@ $SPEC{'handle_cmd'} = {
     args => {
         %App::lcpan::common_args,
         %App::lcpan::scripts_args,
+        %App::lcpan::all_args,
     },
 };
 sub handle_cmd {
@@ -38,18 +39,26 @@ LEFT JOIN file ON script.file_id=file.id
 LEFT JOIN dist ON file.id=dist.file_id
 WHERE script.name IN ($scripts_s)");
 
-    my $res;
+    my @res;
+    my %mem;
+    $sth->execute;
+    while (my $row = $sth->fetchrow_hashref) {
+        unless ($args{all}) {
+            next if $mem{$row->{script}}++;
+        }
+        push @res, $row;
+    }
+
     if (@$scripts == 1) {
-        $sth->execute;
-        (undef, $res) = $sth->fetchrow_array;
-    } else {
-        $sth->execute;
-        $res = {};
-        while (my $row = $sth->fetchrow_hashref) {
-            $res->{$row->{script}} = $row->{dist};
+        @res = map { $_->{dist} } @res;
+        if (@res == 1) {
+            return [200, "OK", $res[0]];
+        } else {
+            return [200, "OK", \@res];
         }
     }
-    [200, "OK", $res];
+
+    [200, "OK", \@res, {'table.fields'=>[qw/script dist/]}];
 }
 
 1;
