f4c4f12eed1f57dfbf4a80e45d6cbf6b3b6c9f26
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-10 14:07:09 +0700

    Add subcommand: mentions-for-all-mods

---
+++ lib/App/lcpan/Cmd/mentions_for_all_mods.pm
@@ -0,0 +1,54 @@
+package App::lcpan::Cmd::mentions_for_all_mods;
+
+# DATE
+# VERSION
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any::IfLOG '$log';
+
+require App::lcpan;
+require App::lcpan::Cmd::mentions_for_mod;
+
+our %SPEC;
+
+my $mentions_for_mod_args = $App::lcpan::Cmd::mentions_for_mod::SPEC{handle_cmd}{args};
+
+$SPEC{'handle_cmd'} = {
+    v => 1.1,
+    summary => 'List PODs which mention all specified module(s)',
+    args => $mentions_for_mod_args,
+};
+sub handle_cmd {
+    my %args = @_;
+
+    my $mres = App::lcpan::Cmd::mentions_for_mod::handle_cmd(%args);
+    return $mres unless $mres->[0] == 200;
+
+    my $mods = $args{modules};
+
+    my %counts; # key = content_path, value = hash of module name => count
+    my %content_data; # key = content_path, value = data
+    for my $e (@{ $mres->[2] }) {
+        $counts{$e->{content_path}}{$e->{module}}++;
+        $content_data{$e->{content_path}} //= {
+            release          => $e->{release},
+            mentioner_author => $e->{mentioner_author},
+            content_path     => $e->{content_path},
+        };
+    }
+
+    my $resmeta = {'table.fields' => [qw/content_path mentioner_author release/]};
+
+    my @res;
+    for my $cp (sort keys %counts) {
+        next unless keys(%{ $counts{$cp} }) == @$mods;
+        push @res, $content_data{$cp};
+    }
+
+    [200, "OK", \@res, $resmeta];
+}
+
+1;
+# ABSTRACT:
