71d58bbe2fefb3a42371fc0d79647f6aefe02ccf
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-26 07:52:08 +0700

    Add subcommand: db-path

---
+++ lib/App/lcpan/Cmd/db_path.pm
@@ -0,0 +1,37 @@
+package App::lcpan::Cmd::db_path;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+require App::lcpan;
+
+our %SPEC;
+
+$SPEC{'handle_cmd'} = {
+    v => 1.1,
+    summary => 'Return database path that is used',
+    description => <<'_',
+
+This is a convenience subcommand for use in, e.g. command-line oneliners.
+
+_
+    args => {
+        %App::lcpan::common_args,
+    },
+    tags => [],
+};
+sub handle_cmd {
+    my %args = @_;
+
+    my $state = App::lcpan::_init(\%args, 'ro');
+    [200, "OK", App::lcpan::_db_path($state->{cpan}, $state->{index_name})];
+}
+
+1;
+# ABSTRACT:
